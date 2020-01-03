5dc8b2c92d6e273398befabafa585534a1821c3e
YordanYordanovGIT <47471839+YordanYordanovGIT@users.noreply.github.com>
2020-01-03 11:25:47 +0200

    Update toneExample.ino

--- examples/toneExample/toneExample.ino
+++ examples/toneExample/toneExample.ino
@@ -8,20 +8,24 @@
  by Yordan Yordanov, BG
 
  GITHUB: https://github.com/YordanYordanovGIT/NewToneLib
-
  */
 
 #include "NewToneLib.h"
 MyTone t(false);
 
 #define buzzerPin 10 //selected pin
+
 void setup() {
   //freq in Hz; min = 31Hz; max = 333Hz
   //duration in milliseconds; max = 4290000‬ms
   //duty cycle is 50%
   //pin shoud be declared as output in setup
   pinMode(buzzerPin, OUTPUT); //pin declared as OUTPUT
-  t.tone(buzzerPin, 4000, 600);//pin, frequency(Hz), duration(ms)
+  t.tone(buzzerPin, 4000, 500);//pin, frequency(Hz), duration(ms)
+  delay(500);
+  t.tone(buzzerPin, 7000, 250);//pin, frequency(Hz), duration(ms)
+  delay(250);
+  t.tone(buzzerPin, 10000, 250);//pin, frequency(Hz), duration(ms)
 }
 
 void loop() {
